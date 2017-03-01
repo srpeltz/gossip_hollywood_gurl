8de34f41c8b5253f7c2eba4fb3551e678707fb0c
steven peltzer <srpeltzer@yahoo.com>
2017-03-01 13:34:56 -0800

    Add About Page and Static Pages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,8 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+
+  get '/about' => 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # resources :users do
